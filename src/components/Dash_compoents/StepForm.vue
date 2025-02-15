cb574fecb51b2f34a29828c12c701c2e334305bb
Nanyian <naoxuan202007@gmail.com>
2025-02-15 16:32:27 +0800

    2/15

--- src/components/Dash_compoents/StepForm.vue
+++ src/components/Dash_compoents/StepForm.vue
@@ -2,27 +2,6 @@
   <div>
     <!-- 步骤1：大纲生成 -->
     <div v-if="activeStep === 0" class="step-form">
-
-
-
-      <!-- 测试接口 -->
-      <!-- Chat  -->
-      <!-- <div class="w-full">
-          <button
-            @click="handleSubmit"
-            :disabled="isPolling || !chatConfig.apiKey || !chatConfig.botId || !chatConfig.message"
-            class="w-full bg-gradient-to-r from-purple-600 to-pink-600 text-white py-2 px-4 rounded-lg hover:opacity-90 transition-opacity disabled:opacity-50"
-          >
-            {{ isPolling ? 'Generating Response...' : 'Send Message' }}
-            
-          </button>
-
-          <ResponseDisplay 
-          :response="response"
-          :messages="chatMessages"
-        />
-      
-        </div> -->
       <!-- chat -->
       <el-form :model="form" label-position="top">
         <el-row :gutter="20" justify="center">
@@ -96,6 +75,7 @@ import ResponseDisplay from '@/components/api_compoents/ResponseDisplay.vue';
 import {
   response, chatMessages, handleSubmit, isPolling, chatConfig
 } from '@/components/api_compoents/api_handler';
+import { generatePlan } from './DashCompoents';
 
 
 
@@ -158,15 +138,15 @@ const imageStyle = computed(() => ({
   cursor: isHovering.value ? 'zoom-in' : 'default',
 }));
 
-const generatePlan = async () => {
-  isGenerating.value = true;
-  setTimeout(() => {
-    isProcessing.value = false;
-    showResult.value = true;
-    console.log(showResult.value);
-    emit('update:showResult', showResult.value);
-  }, props.endWaitingTime);
-};
+// const generatePlan = async () => {
+//   isGenerating.value = true;
+//   setTimeout(() => {
+//     isProcessing.value = false;
+//     showResult.value = true;
+//     console.log(showResult.value);
+//     emit('update:showResult', showResult.value);
+//   }, props.endWaitingTime);
+// };
 
 </script>
 
